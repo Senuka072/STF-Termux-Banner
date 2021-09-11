9874e7633f7a5726afbc84f3b9a593da47419256
Senuka072 <86052269+Senuka072@users.noreply.github.com>
2021-09-11 12:10:36 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,1 +1,32 @@
+import os
+import colorama
+from colorama import Fore
 
+
+os.system("pkg upgrade")
+print(Fore.MAGENTA + "Done Upgrade")
+
+os.system("pkg update")
+print(Fore.MAGENTA + "Done update")
+
+os.system("pkg python")
+print(Fore.MAGENTA + "Done installing Python")
+
+os.system("pkg ruby")
+print(Fore.MAGENTA + "Done installing Rubuy")
+
+os.system("pkg install figlet")
+os.system("figlet Done installing figlet")
+                                                                                
+os.system("gem install lolcat")
+os.system("figlet Done installing Lolcat | lolcat")                             
+os.system("pkg install cowsay")                                                 
+os.system("cowsay Done installing Cowsay | lolcat")
+
+os.system("pip install colorama")
+os.system("figlet Done installing Colorama | lolcat")
+
+os.system("pip install neofetch")
+os.system("figlter Done installing Neofetch | lolcat")
+
+os.system("figlet Done installing All Packages | lolcat")
